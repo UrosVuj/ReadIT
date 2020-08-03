663ce2a53372f5de1d225610c972e34dde410f28
Uroš Vujošević <UrosVuj@users.noreply.github.com>
2020-08-04 00:04:50 +0200

    user registration underway

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User');
+
+module.exports.register = (req, res, next) => {
+
+    var user = new User();
+    user.first_name = req.body.first_name;
+    user.last_name = req.body.last_name;
+    user.dob = req.body.dob;
+    user.country = req.body.country;
+    user.city = req.body.city;
+    user.username = req.body.username;
+    user.email = req.body.email;
+    user.password = req.body.password;
+
+    user.save((err, doc) => {
+        if (!err)
+            res.send(doc);
+        else {
+            if (err.code == 11000)
+                res.status(422).send(['Duplicate email adrress found.']);
+            else
+                return next(err);
+        }
+
+    });
+}
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "welcome", component: WelcomeComponent },
+  { path: "", component: WelcomeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
